669a554b22dacf72feb34085cdc78cd10da181c3
dcode2100 <dikshantsolanki214@gmail.com>
2024-10-27 20:03:40 +0530

    Few updates

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,60 +1,55 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import Image from 'next/image';
 import styles from "@styles/Navbar.module.scss";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
-const navItems = [
-  {
-    path: "/",
-    name: "Home",
-  },
-  {
-    path: "/products",
-    name: "Products",
-  },
-];
-
-function Navbar() {
+const Navbar = () => {
   const [menuActive, setMenuActive] = useState(false);
-  let pathname = usePathname() ;
-
+  const pathname = usePathname();
   const [hoveredPath, setHoveredPath] = useState(pathname);
+
+  const navItems = useMemo(() => [
+    { path: "/", name: "Home" },
+    { path: "/products", name: "Products" },
+  ], []);
 
   return (
     <nav className='navbar flex justify-between items-center py-[1.5rem] md:py-[1.5rem] max-xs:px-[1.5rem] md:fixed top-0 w-full bg-transparent bg-white px-6 md:px-[7rem] z-[100]'>
-      <a href='/'><Image className="brand_logo max-md:top-0.5" src="/evakey_logo.svg" width={200} height={80} quality={100} alt='evakeylogo' /></a>
+      <Link href='/' aria-label="Homepage">
+        <Image className="brand_logo max-md:top-0.5" src="/evakey_logo.svg" width={200} height={80} quality={100} alt='evakeylogo' />
+      </Link>
 
       <div className='flex text-black space-x-4'>
         <div className="border border-stone-800/90 p-[0.4rem] max-md:hidden rounded-lg sticky top-4 z-[100] bg-stone-900/80 backdrop-blur-md">
           <nav className="flex gap-2 relative justify-start w-full z-[100] rounded-lg">
-            {navItems.map((item, index) => {
-              const isActive = item.path === pathname;
+            <ul className="flex space-x-4">
+              {navItems.map((item) => {
+                const isActive = item.path === pathname;
 
-              return (
-                <Link
-                  key={item.path}
-                  className={`px-4 py-2 rounded-md text-sm lg:text-base relative no-underline duration-300 ease-in ${isActive ? "text-zinc-100" : "text-zinc-400"}`}
-                  data-active={isActive}
-                  href={item.path}
-                  onMouseOver={() => setHoveredPath(item.path)}
-                  onMouseLeave={() => setHoveredPath(pathname)}
-                >
-                  <span>{item.name}</span>
-                  {item.path === hoveredPath && (
-                    <div
-                      className="absolute bottom-0 left-0 h-full bg-stone-800/80 rounded-md -z-10"
-                      layoutid="navbar"
-                      aria-hidden="true"
-                      style={{
-                        width: "100%",
-                      }}
-                    />
-                  )}
-                </Link>
-              );
-            })}
+                return (
+                  <li key={item.path}>
+                    <Link
+                      className={`px-4 py-2 rounded-md text-sm lg:text-base relative no-underline duration-300 ease-in ${isActive ? "text-zinc-100" : "text-zinc-400"}`}
+                      data-active={isActive}
+                      href={item.path}
+                      onMouseOver={() => setHoveredPath(item.path)}
+                      onMouseLeave={() => setHoveredPath(pathname)}
+                    >
+                      <span>{item.name}</span>
+                      {item.path === hoveredPath && (
+                        <div
+                          className="absolute bottom-0 left-0 h-full bg-stone-800/80 rounded-md -z-10"
+                          aria-hidden="true"
+                          style={{ width: "100%" }}
+                        />
+                      )}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
           </nav>
         </div>
         <div className="md:hidden flex items-center">
@@ -62,7 +57,7 @@ function Navbar() {
             className={`${styles.box} ${menuActive ? styles.active : ''}`}
             onClick={() => setMenuActive(!menuActive)}
             type="button"
-            aria-label="hamburger menu"
+            aria-label="Toggle menu"
           >
             <div className={styles.hamburguer}>
               {/* Your hamburger menu content */}
@@ -74,7 +69,7 @@ function Navbar() {
         className={`${styles.mobile} ${menuActive ? styles.on : styles.off} md:hidden bg-opacity-20`}
       >
         <div className={`flex-col md:hidden gap-3 justify-end ${menuActive && "flex"} text-right m-0 relative z-10`}>
-          {navItems.map((item, index) => (
+          {navItems.map((item) => (
             <Link key={item.path} href={item.path}>
               <div
                 onClick={() => setMenuActive(!menuActive)}
@@ -88,6 +83,6 @@ function Navbar() {
       </aside>
     </nav>
   );
-}
+};
 
 export default Navbar;
