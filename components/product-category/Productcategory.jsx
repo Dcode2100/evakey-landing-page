adafed766c26c82d5b34aa28351fe2f123aac998
dcode2100 <dikshantsolanki214@gmail.com>
2024-10-31 13:57:45 +0530

    Fix the cards

--- components/product-category/Productcategory.jsx
+++ components/product-category/Productcategory.jsx
@@ -7,26 +7,26 @@ const ProductCategory = () => {
       id: 1,
       name: "Product 1",
       bgColor: "bg-red-300",
-      extracolors: "bg-blue-600",
-      image: "/Assets/promotional-keyrings.png", // Updated path
+      extracolors: "opacity-50 bg-black",
+      image: "/Assets/promotional-keyrings.png", 
     },
     {
       id: 2,
       name: "Product 2",
       bgColor: "bg-green-300",
-      extracolors: "bg-yellow-600",
+      extracolors: "opacity-50 bg-black",
     },
     {
       id: 3,
       name: "Product 3",
       bgColor: "bg-blue-300",
-      extracolors: "bg-red-600",
+      extracolors: "opacity-50 bg-black",
     },
     {
       id: 4,
       name: "Product 4",
       bgColor: "bg-yellow-300",
-      extracolors: "bg-purple-600",
+      extracolors: "opacity-50 bg-black",
     },
   ];
 
@@ -38,24 +38,24 @@ const ProductCategory = () => {
       <h1 className="text-sm pt-3">
         Feature your logo and promote your brand to clients across INDIA
       </h1>
-      <div className="h-[32rem] mt-9 flex flex-wrap gap-10 pl-14">
+      <div className="h-[32rem] mt-9 flex flex-wrap rounded-xl overflow-hidden gap-10 pl-14">
         {Datas.map((data) => (
           <div
             key={data.id}
-            className={`h-[15rem] w-[15rem] ${data.bgColor} rounded-xl flex flex-col`}
+            className={`h-[15rem] relative w-[15rem] ${data.bgColor} rounded-xl flex flex-col`}
           >
+            {data?.image && (
+              <Image
+                className="absolute top-0 left-0 w-full h-full object-cover rounded-xl"
+                src={data.image}
+                alt={data.name}
+                width={100}
+                height={100}
+              />
+            )}
             <div
-              className={`w-full ${data.extracolors} h-[9vh] mt-auto rounded-b-xl flex justify-center items-center`}
+              className={`w-full ${data.extracolors} h-[9vh] relative mt-auto rounded-b-xl flex justify-center items-center`}
             >
-              {data.image && (
-                <Image
-                  className=""
-                  src={data.image}
-                  alt={data.name}
-                  width={100} // Set appropriate width
-                  height={100} // Set appropriate height
-                />
-              )}
               <h1 className="text-white font-black text-xl">{data.name}</h1>
             </div>
           </div>
