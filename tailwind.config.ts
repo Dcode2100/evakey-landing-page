caec93d795180eb53858304d5cba182f14f44fdc
dcode2100 <dikshantsolanki214@gmail.com>
2024-11-11 17:36:50 +0530

    adds New Fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,7 +55,10 @@ const config: Config = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+  		fontFamily: {
+  			roboto: ['var(--font-roboto-flex)'],
+  		},
   	}
   },
   plugins: [require("tailwindcss-animate")],
